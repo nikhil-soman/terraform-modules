08d93ec50d1faada2aef5ae1f40ff218df21e1a5
nikhil soman <nsoman@nikhils-MacBook-Air.local>
2021-02-21 00:22:11 +0530

    Production relase of the webserver-rds-cluster

--- services/webserver-cluster/user-data.sh
+++ services/webserver-cluster/user-data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cat > index.html <<EOF
-<h1> Hello, world </h1>
+<h1> Hello, world. This is Production website </h1>
 <p> DB endpint: ${db_address} </p>
 <p> DB port: ${db_port} </p>
 EOF
